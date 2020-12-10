146b49f2d6d41562776d4f3d4b2720acc663b41a
jmos1 <jmorar567@gmail.com>
2020-12-10 13:01:14 -0500

    Fixed block comments to make it easier to read

--- server/server.c
+++ server/server.c
@@ -5,11 +5,11 @@
  *
  * @brief  A multithreaded server module which demonstrates the use
  *  	   of thread pools, condition variables, fifo buffers, and sockets
- * 		   among other things.
- *  	  	 The client transmits a greeting, and the server replies with its
+ * 	   among other things.
+ *  	     The client transmits a greeting, and the server replies with its
  *  	   own greeting. If all threads are busy and the fifo is full, then the
  *  	   main thread processes the client request and transmits a message
- * 	 	   indicating that the server is full. Each thread sleeps for 5 seconds
+ * 	   indicating that the server is full. Each thread sleeps for 5 seconds
  *  	   to allow the fifo to fill.
  */
 #include <stdio.h>
@@ -19,7 +19,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h> 	/* Mutex locks and threads */
-#include <unistd.h>		/* Sleep */
+#include <unistd.h>	/* Sleep */
 #include "fifo.h"
 
 #define IP_ADDR				"127.0.0.1"
@@ -191,7 +191,7 @@ serv_handle_t *server_start()
  * 
  * @param  socket Pointer to client socket which should be in fifo node.
  * @param  txBuf Pointer to buffer that contains message that will be sent
- * 		   to client.
+ * 	   to client.
  * @param  bufSize Size of txBuf.
  * @return none
  */
@@ -210,7 +210,7 @@ void server_send(int *socket, char *txBuf, ssize_t bufSize)
  *
  * @param  socket Pointer to client socket which should be in fifo node.
  * @param  rxBuf Pointer to buffer that contains message that will be 
- * 		   received by the server.
+ * 	   received by the server.
  * @param  bufSize Size of rxBuf.
  * @return none
  */
